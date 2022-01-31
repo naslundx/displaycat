04aa596512ccb2a0fa40995585e3386d4987502a
Marcus Näslund <naslundx@gmail.com>
2022-01-31 20:05:37 +0100

    Does this solve routes on heroku?

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use('/', express.static('build/'));
 app.use('/old', express.static('old/'));
+app.use('/', express.static('build/'));
 
 app.listen(process.env.PORT || 5000);
