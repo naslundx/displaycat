a110c23bcfb2416a95e6487688d33b1401b498e5
Marcus Näslund <naslundx@gmail.com>
2022-01-31 10:21:35 +0100

    Added first react prototype

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /*jshint globalstrict: true*/
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
+app.use(express.static('public/react'));
 app.listen(process.env.PORT || 5000);
