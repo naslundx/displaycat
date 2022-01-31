dae50893292a9774f146252efb6fe48e76061dbb
Marcus Näslund <naslundx@gmail.com>
2022-01-31 19:57:39 +0100

    changed bg for react version

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('build/'));
+app.use('/', express.static('build/'));
 app.use('/old', express.static('old/'));
+
 app.listen(process.env.PORT || 5000);
