491f87b53ff3cf220d934f90845c106f1c53976a
Marcus Näslund <naslundx@gmail.com>
2022-02-01 15:13:26 +0100

    Faster loading

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 /*jshint globalstrict: true*/
+const compression = require('compression');
 const express = require('express');
 const app = express();
+
+app.use(compression());
+
+app.disable('x-powered-by');  
 
 app.use('//old', express.static('old/'));
 app.use('/old', express.static('old/'));
